78fa63762d4bb83583f5b9f5dc65b2d70dbda11c
sebataranto1365 <74457706+sebataranto1365@users.noreply.github.com>
2021-03-01 21:55:55 -0300

    script campos

---
+++ js/buscador.js
@@ -0,0 +1,73 @@
+let departamento = prompt("Ingrese departamento:")
+let localidad = prompt("Ingrese localidad:")
+let coneat = prompt("Ingrese indice coneat:")
+let hectareas = prompt("Ingrese hectareas:")
+
+
+
+switch(departamento) {
+    case "Artigas":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Canelones":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Cerro Largo":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Colonia":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Durazno":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Flores":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Florida":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Florida":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Lavalleja":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Maldonado":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Montevideo":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Paysandu":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Rio Negro":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Rivera":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Rocha":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Salto":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "San Jose":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Soriano":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Tacuarembo":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    case "Treinta y Tres":
+    alert("Nuestro representante se pondra en contacto con usted lo antes posible")
+break
+    default: 
+    alert("No se encuentran resultados a su busqueda")
+    console.warn("No se encuentran resultados a su busqueda")
+break
+}
